66bdb56e84ebb3420b38c4cc5224c3d4b6841696
Tugberk Ugurlu <tugberk@outlook.com>
2015-10-01 17:44:05 +0100

    initial commit

---
+++ src/MongoDB.Context/MongoContext.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Concurrent;
+using MongoDB.Driver;
+
+namespace MongoDB.Context 
+{
+	public abstract class MongoCqrsContext
+	{
+		private readonly ConcurrentDictionary<Type, object> _mongoCollections;
+		private readonly IMongoDatabase _database;
+		
+		public MongoCqrsContext(IMongoDatabase database)
+		{
+            if (database == null)
+			{
+				throw new ArgumentNullException("database");	
+			}
+			
+			_database = database;
+			_mongoCollections = new ConcurrentDictionary<Type, object>();
+        }
+		
+        protected IMongoCollection<TEntity> InitializeCollection<TEntity>(string collectionName)
+        {
+            if (collectionName == null)
+			{
+				throw new ArgumentNullException("collectionName");	
+			}
+			
+            return (IMongoCollection<TEntity>)_mongoCollections.GetOrAdd(typeof(TEntity), k => _database.GetCollection<TEntity>(collectionName));
+        }
+	}
+}
